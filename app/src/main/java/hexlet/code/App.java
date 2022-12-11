995e414c407666e4c00228313ce6c1804edfb7c3
Ivan <ivanprikhodko2004@gmail.com>
2022-12-11 15:39:13 +0300

    big fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,50 +4,43 @@ import hexlet.code.games.*;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        var exit = true;
-        while (exit) {
-            System.out.print("Please enter the game number and press Enter.\n"
-                    + "1 - Greet\n"
-                    + "2 - Even\n"
-                    + "3 - Calc\n"
-                    + "4 - GCD\n"
-                    + "5 - Progression\n"
-                    + "6 - Prime\n"
-                    + "0 - Exit\n"
-                    + "Your choice: ");
+            System.out.print("""
+                    Please enter the game number and press Enter.
+                    1 - Greet
+                    2 - Even
+                    3 - Calc
+                    4 - GCD
+                    5 - Progression
+                    6 - Prime
+                    0 - Exit
+                    Your choice:\s""");
             var choice = Integer.parseInt(scanner.next());
-            System.out.print("\n")
+            System.out.print("\n");
             switch (choice) {
-                case 1:
+                case 1 ->
                     //приветствие
-                    Engine.hello();
-                    break;
-                case 2:
+                        Engine.hello();
+                case 2 ->
                     //игра четность
-                    ParityCheck.start();
-                    break;
-                case 3:
+                        ParityCheck.start();
+                case 3 ->
                     //игра калькулятор
-                    Calculator.start();
-                    break;
-                case 4:
+                        Calculator.start();
+                case 4 ->
                     //игра НОД
-                    GCD.start();
-                    break;
-                case 5:
+                        GCD.start();
+                case 5 ->
                     //игра прогрессия
-                    Progression.start();
-                    break;
-                case 6:
+                        Progression.start();
+                case 6 ->
                     //игра простые числа
-                    Prime.start();
-                    break;
-                case 0:
-                    exit = false;
-                    break;
-                default:
-                    break;
+                        Prime.start();
+                case 0 -> {
+                    return;
+                }
+                default -> {
+                    return;
+                }
             }
-        }
     }
 }
